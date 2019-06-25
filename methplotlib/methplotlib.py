4d006d88dfbd80efb96a28d539f242291f48e08c
wdecoster <decosterwouter@gmail.com>
2019-06-25 17:26:17 +0200

    adding docstring and minor refactoring

--- methplotlib/methplotlib.py
+++ methplotlib/methplotlib.py
@@ -23,26 +23,34 @@ def meth_browser(methlist, names, window, gtf=False, smoothen=5, simplify=False,
     methlist is a list of files from calculate_methylation_frequency
     names should have the same length as methlist and contain identifiers for the datasets
     annotation is optional and is a gtf, which will be processed by parse_gtf()
+
+    if the traces are split per sample,
+     then show one line per sample and one for the annotation
+    if no splitting is done,
+     then 4/5 of the browser is used for overlayed samples and one for annotation
+    the trace to be used for annotation is thus methrows + 1
     """
     if split:
         methrows = len(methlist)
-        layout = [[{}] for i in range(methrows + 1)]
         annot_axis = 'yaxis{}'.format(methrows + 1)
-    else:
-        methrows = 4
-        layout = [[{'rowspan': methrows}], [None], [None], [None], [{}], ]
-        annot_axis = 'yaxis2'
-    fig = tools.make_subplots(rows=methrows + 1,
-                              cols=1,
-                              shared_xaxes=True,
-                              specs=layout,
-                              print_grid=False
-                              )
-    if split:
+        fig = tools.make_subplots(rows=methrows + 1,
+                                  cols=1,
+                                  shared_xaxes=True,
+                                  specs=[[{}] for i in range(methrows + 1)],
+                                  print_grid=False
+                                  )
         for position, meth_trace in enumerate(plots.methylation(methlist, names, window, smoothen),
                                               start=1):
             fig.append_trace(trace=meth_trace, row=position, col=1)
     else:
+        methrows = 4
+        annot_axis = 'yaxis2'
+        fig = tools.make_subplots(rows=methrows + 1,
+                                  cols=1,
+                                  shared_xaxes=True,
+                                  specs=[[{'rowspan': methrows}], [None], [None], [None], [{}], ],
+                                  print_grid=False
+                                  )
         for meth_trace in plots.methylation(methlist, names, window, smoothen):
             fig.append_trace(trace=meth_trace, row=1, col=1)
     fig["layout"]["xaxis"].update(tickformat='g', separatethousands=True)

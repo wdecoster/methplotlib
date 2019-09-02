2d9414a9802677e8fcec80fd0a3d98d2f13b182a
wdecoster <decosterwouter@gmail.com>
2019-09-02 13:13:21 +0200

    joining dataframes after collecting all information

--- scripts/split_calls_by_phase.py
+++ scripts/split_calls_by_phase.py
@@ -6,25 +6,19 @@ import pandas as pd
 def main():
     args = get_args()
     calls = pd.read_csv(args.methylation, sep="\t")
-    calls['PS'] = None
-    calls['HP'] = None
-    for read in pysam.AlignmentFile(args.bam).fetch():
-        if read.has_tag('PS'):
-            calls.loc[calls['read_name'] == read.query_name, ["PS", "HP"]] = (
-                read.get_tag('PS'), read.get_tag('HP'))
-    if args.output == 'stdout':
-        print(calls.to_csv(path_or_buf=None, sep="\t", na_rep="NaN", index=False))
-    else:
-        calls.to_csv(path_or_buf=args.output, sep="\t", na_rep="NaN", index=False)
+    phase_info = pd.DataFrame(
+        data=[(read.query_name, read.get_tag('PS'), read.get_tag('HP'))
+              for read in pysam.AlignmentFile(args.bam).fetch() if read.has_tag('PS')],
+        columns=['read_name', 'PS', 'HP']) \
+        .set_index('read_name')
+    print(calls.join(phase_info, by='read_name')
+               .to_csv(path_or_buf=None, sep="\t", na_rep="NaN", index=False))
 
 
 def get_args():
     parser = ArgumentParser(description="Split a nanopolish call-methylation file by haplotypes")
     parser.add_argument("methylation", help="File created by nanopolish call-methylation")
     parser.add_argument("bam", help="bam file created by whatshap haplotag")
-    parser.add_argument("-o", "--output",
-                        help="Output file to write to. Default is to stdout.",
-                        default='stdout')
     return parser.parse_args()
 
 

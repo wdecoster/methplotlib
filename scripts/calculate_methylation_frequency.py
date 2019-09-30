c6283950eeca4a148f1ee21af0986b532c0a2fc7
wdecoster <decosterwouter@gmail.com>
2019-09-30 12:29:58 +0200

    refactored

--- scripts/calculate_methylation_frequency.py
+++ scripts/calculate_methylation_frequency.py
@@ -29,65 +29,66 @@ def update_call_stats(key, num_called_cpg_sites, is_methylated, sequence):
         sites[key].called_sites_methylated += num_called_cpg_sites
 
 
-parser = argparse.ArgumentParser(description='Calculate methylation frequency at genomic CpG sites')
-parser.add_argument('-c', '--call-threshold', type=float, required=False, default=2.5)
-parser.add_argument('-i', '--input', type=str, required=False)
-parser.add_argument('-s', '--split-groups', action='store_true')
-args = parser.parse_args()
-assert(args.call_threshold is not None)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Calculate methylation frequency per site')
+    parser.add_argument('-c', '--call-threshold', type=float, required=False, default=2.5)
+    parser.add_argument('-i', '--input', type=str, required=False)
+    parser.add_argument('-s', '--split-groups', action='store_true')
+    args = parser.parse_args()
+    assert(args.call_threshold is not None)
 
-sites = dict()
+    sites = dict()
 
-if args.input:
-    if args.input.endswith(".gz"):
-        in_fh = gzip.open(args.input, 'rt')
+    if args.input:
+        if args.input.endswith(".gz"):
+            in_fh = gzip.open(args.input, 'rt')
+        else:
+            in_fh = open(args.input)
     else:
-        in_fh = open(args.input)
-else:
-    in_fh = sys.stdin
-csv_reader = csv.DictReader(in_fh, delimiter='\t')
+        in_fh = sys.stdin
 
-for record in csv_reader:
+    for record in csv.DictReader(in_fh, delimiter='\t'):
 
-    num_sites = int(record['num_motifs'])
-    llr = float(record['log_lik_ratio'])
+        num_sites = int(record['num_motifs'])
+        llr = float(record['log_lik_ratio'])
 
-    # Skip ambiguous call
-    if abs(llr) < args.call_threshold:
-        continue
-    sequence = record['sequence']
-
-    is_methylated = llr > 0
-
-    # if this is a multi-cpg group and split_groups is set, break up these sites
-    if args.split_groups and num_sites > 1:
-        c = str(record['chromosome'])
-        s = int(record['start'])
-        e = int(record['end'])
-
-        # find the position of the first CG dinucleotide
+        # Skip ambiguous call
+        if abs(llr) < args.call_threshold:
+            continue
         sequence = record['sequence']
-        cg_pos = sequence.find("CG")
-        first_cg_pos = cg_pos
-        while cg_pos != -1:
-            key = (c, s + cg_pos - first_cg_pos, s + cg_pos - first_cg_pos)
-            update_call_stats(key, 1, is_methylated, "split-group")
-            cg_pos = sequence.find("CG", cg_pos + 1)
-    else:
-        key = (str(record['chromosome']), int(record['start']), int(record['end']))
-        update_call_stats(key, num_sites, is_methylated, sequence)
 
-# header
-print("\t".join(["chromosome", "start", "end", "num_motifs_in_group", "called_sites",
-                 "called_sites_methylated", "methylated_frequency", "group_sequence"]))
+        is_methylated = llr > 0
 
-sorted_keys = sorted(sites.keys(), key=lambda x: x)
-if len(sorted_keys) == 0:
-    sys.exit("ERROR: No sites found for calculating frequencies!")
+        # if this is a multi-cpg group and split_groups is set, break up these sites
+        if args.split_groups and num_sites > 1:
+            c = str(record['chromosome'])
+            s = int(record['start'])
+            e = int(record['end'])
 
-for key in sorted_keys:
-    if sites[key].called_sites > 0:
-        (c, s, e) = key
-        f = float(sites[key].called_sites_methylated) / sites[key].called_sites
-        print("%s\t%s\t%s\t%d\t%d\t%d\t%.3f\t%s" % (
-            c, s, e, sites[key].group_size, sites[key].called_sites, sites[key].called_sites_methylated, f, sites[key].sequence))
+            # find the position of the first CG dinucleotide
+            sequence = record['sequence']
+            cg_pos = sequence.find("CG")
+            first_cg_pos = cg_pos
+            while cg_pos != -1:
+                key = (c, s + cg_pos - first_cg_pos, s + cg_pos - first_cg_pos)
+                update_call_stats(key, 1, is_methylated, "split-group")
+                cg_pos = sequence.find("CG", cg_pos + 1)
+        else:
+            key = (str(record['chromosome']), int(record['start']), int(record['end']))
+            update_call_stats(key, num_sites, is_methylated, sequence)
+
+    # header
+    print("\t".join(["chromosome", "start", "end", "num_motifs_in_group", "called_sites",
+                     "called_sites_methylated", "methylated_frequency", "group_sequence"]))
+
+    sorted_keys = sorted(sites.keys(), key=lambda x: x)
+    if len(sorted_keys) == 0:
+        sys.exit("ERROR: No sites found for calculating frequencies!")
+
+    for key in sorted_keys:
+        if sites[key].called_sites > 0:
+            (c, s, e) = key
+            f = float(sites[key].called_sites_methylated) / sites[key].called_sites
+            print("%s\t%s\t%s\t%d\t%d\t%d\t%.3f\t%s" % (
+                c, s, e, sites[key].group_size, sites[key].called_sites, sites[key].called_sites_methylated, f, sites[key].sequence))

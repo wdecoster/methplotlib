d46926db33f3d7f6e07a972dbeb4c04b2e7fc7a1
wdecoster <decosterwouter@gmail.com>
2019-11-15 15:15:09 +0100

    fix line too long

--- methplotlib/helpers.py
+++ methplotlib/helpers.py
@@ -4,6 +4,8 @@ from pyranges import PyRanges
 
 def methylation_pyranges_from_csv(inputfile):
     colnames = ["Chromosome", "Start", "End", "calls", "methylated"]
-    return PyRanges(pd.read_csv(inputfile, sep="\t", names=colnames, header=0, usecols=[0, 1, 2, 4, 5]))
-
-
+    return PyRanges(pd.read_csv(inputfile,
+                                sep="\t",
+                                names=colnames,
+                                header=0,
+                                usecols=[0, 1, 2, 4, 5]))

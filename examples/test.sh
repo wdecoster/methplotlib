766a96c61c2b1ed816c3450404e58366a761f01b
Endre Bakken Stovner <endrebak85@gmail.com>
2019-11-21 10:51:42 +0100

    Fix script in examples

--- examples/test.sh
+++ examples/test.sh
@@ -1,9 +1,9 @@
 set -ev
 
 methplotlib -h
-methplotlib -m test/NA19240-methylation_ACTB_calls.tsv.gz test/NA19240-methylation_ACTB_frequency.tsv.gz \
+methplotlib -m examples/NA19240-methylation_ACTB_calls.tsv.gz examples/NA19240-methylation_ACTB_frequency.tsv.gz \
             -n calls frequencies \
             -w chr7:5,525,542-5,543,028 \
-            -g test/GRCh38-ACTB-locus.gtf.gz \
+            -g examples/GRCh38-ACTB-locus.gtf.gz \
             --simplify \
-            -b test/DNase_cluster_ACTB.bed.gz
+            -b examples/DNase_cluster_ACTB.bed.gz
